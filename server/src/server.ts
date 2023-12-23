36c356b5fc660bc11543b8c0589ad42187cd6158
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-12-22 22:37:09 -0300

    commit n38
    add pag server earth

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import { fastifyCors } from "@fastify/cors"
 import { RouteApod } from "./routes/apod"
 import { RouteNeoWs } from "./routes/neows"
 import { RouteDonki } from "./routes/donki"
+import { RouteEarth } from "./routes/earth"
 import "dotenv/config"
 
 const app = fastify()
@@ -15,6 +16,7 @@ app.register(fastifyCors, {
 app.register(RouteApod)
 app.register(RouteNeoWs)
 app.register(RouteDonki)
+app.register(RouteEarth)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Servidor rodando na porta: http://localhost:3333")
