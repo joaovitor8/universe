470bde8ed7cb66076a8c390654003174054f669e
João Vitor <joaoezeki@gmail.com>
2024-07-21 22:11:16 -0300

    V 0.8.4

--- front/src/components/componentsPages/solarSystem/System.tsx
+++ front/src/components/componentsPages/solarSystem/System.tsx
@@ -35,9 +35,9 @@ export const System = () => {
                   <CardTitle>{d.name}</CardTitle>
                   <CardDescription>{d.id}° planet after the sun</CardDescription>
                 </CardHeader>
-                <CardContent className="flex   sm:flex-col sm:space-y-5   lg:flex-row lg:items-center lg:space-x-10">
-                  <div className="w-full   sm:flex sm:justify-center">
-                    <img src={d.image} alt={d.name} className="h-[400px] w-[400px]"/>
+                <CardContent className="flex   sm:flex-col sm:space-y-5   lg:flex-row ">
+                  <div className="sm:w-full sm:flex sm:justify-center   lg:w-1/2">
+                    <img src={d.image} alt={d.name} className="h-[300px] w-[300px]"/>
                   </div>
 
                   <div className="space-y-1">
@@ -48,7 +48,6 @@ export const System = () => {
                     <p><span className="text-violet-700 font-bold">Average Temperature Celsius:</span> {d.average_celsius_temperature}</p>
                     <p><span className="text-violet-700 font-bold">Composition:</span> {d.composition}</p>
                     <p><span className="text-violet-700 font-bold">Distance to the Sun:</span> {d.distance_to_the_sun}</p>
-                    <p><span className="text-violet-700 font-bold">Characteristics:</span> {d.characteristics}</p>
                     <p><span className="text-violet-700 font-bold">satellites:</span> {d.satellites}</p>
                   </div>
                 </CardContent>
