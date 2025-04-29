1f9c59719d3819960ced001d3821e43ee10da085
João Vitor <joaoezeki@gmail.com>
2025-04-28 23:21:29 -0300

    V 4.4.5

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -2,7 +2,7 @@
 export default function NotFound() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h2 className="text-purple-700 font-bold">Page not found: Erro 404</h2>
+      <h2 className="text-purple-700 font-bold text-5xl">Page not found: Erro 404</h2>
     </div>
   )
 }
